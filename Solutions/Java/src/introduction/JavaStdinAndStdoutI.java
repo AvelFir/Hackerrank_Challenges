a7534cdf0e069b56b383b43a1fb5dc239292ad5c
AvelFir <guichasemartinsgmr@hotmail.com>
2020-11-11 22:20:39 -0300

    Added Close()

--- Solutions/Java/src/introduction/JavaStdinAndStdoutI.java
+++ Solutions/Java/src/introduction/JavaStdinAndStdoutI.java
@@ -13,6 +13,9 @@ public class JavaStdinAndStdoutI {
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		int c = scan.nextInt();
+		
+		scan.close();
+		
 		System.out.println(a);
 		System.out.println(b);
 		System.out.println(c);
